0acd835264e9a8460db762ca21bc65c7d297291d
eya gueddes <eyagueddes1@gmail.com>
2021-11-20 01:00:48 +0100

    movies-list

---
+++ src/common/apis/MovieApiKey.js
@@ -0,0 +1,1 @@
+export const APIKey="184aa46e";
---
+++ src/components/Home/Home.js
@@ -0,0 +1,28 @@
+import React ,{useEffect} from 'react';
+import  MovieListing from "../MovieListing/MovieListing";
+import movieApi from "../../common/apis/MovieApi";
+import {APIKey} from "../../common/apis/MovieApiKey";
+import { addMovies } from '../../features/movies/movieSlice';
+import { useDispatch } from 'react-redux';
+const Home = () => {
+    const movieText = "Harry";
+    const dispatch = useDispatch();
+        useEffect(() => {
+        const fetchMovies = async() => {
+            const response= await movieApi.get(`?apikey=${APIKey}&s=${movieText}&type=movie`).catch((err)=>{
+                console.log("Err:",err);
+            });
+            dispatch(addMovies(response.data));
+        };
+        fetchMovies();
+        
+    }, []);
+    return (
+        <div>
+            <div className="banner-img"></div>
+            <MovieListing/>
+        </div>
+    );
+};
+
+export default Home; 
